f10f5cfb00ed414c038b223634673b192016c05d
Pranith Gunda <pranith0527@yahoo.com>
2024-03-25 00:59:12 -0400

    Updated adding notes for JavaScript, added functions in Script.js file

--- assets/script.js
+++ assets/script.js
@@ -33,8 +33,41 @@ that is related in some way. */
 
 // 
 
-const topics = ["HTML","CSS","Git","JavaScript"];
+// Functions are reusable blocks of code that contain instructions to perform a specific task
+// To create a function, we use function keyword, similar to how we used const or let keyword and give our function a unique name.
+// A functions name is followed by parantheses(). 
 
-for(let i=0; i<topics.length;i++){
-    console.log(topics[i]);
+
+
+const topics = ['HTML','CSS','Git','JavaScript'];
+
+const randomTopic = topics[Math.floor(Math.random() * topics.length)];
+
+function listTopics(){
+
+    for(let i=0; i<topics.length;i++){
+        console.log(topics[i]);
+    }
+
 }
+
+function selectTopic(){
+
+    if (randomTopic === 'HTML') {
+        console.log("Lets study HTML!")
+    } else if (randomTopic === 'CSS') {
+        console.log("Let's study CSS!")
+    } else if (randomTopic === 'Git') {
+        console.log("Let's study Git!");
+    } else if (randomTopic === 'JavaScript') {
+        console.log("Let's study JavaScript!")
+    } else {
+        console.log('Please try again');
+    }
+
+}
+console.log('Here are the topics we learned through Prework:');
+listTopics();
+
+console.log('Which topic should we study first?');
+selectTopic();
